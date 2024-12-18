99792941688fb494769a8afa401e48a707e539f0
Eloi <reo.developer@gmail.com>
2024-12-18 14:38:28 +0100

    deploy method change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://adriaperezpla.github.io',
-	base: 'adriaperezpla.github.io',
 	integrations: [tailwind()],
 });
